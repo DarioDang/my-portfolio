47eaaf99c49e53e5e35d793df67965ece7254034
DarioDang <ankhuong9999@gmail.com>
2025-08-11 22:52:18 +1200

    Initial commit

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')  # Homepage
+
+@app.route('/taxi_ride')
+def taxi_ride():
+    return render_template('taxi_ride.html')  # Project detail page
+
+@app.route('/student_retention')
+def student_retention():
+    return render_template('student_retention.html')
+
+@app.route('/geospatial_crash')
+def geospatial_crash():
+    return render_template('geospatial_crash.html')
+
+@app.route('/projects/million-songs')
+def million_songs():
+    return render_template('million_songs.html')
+
+@app.route('/precipitation-project')
+def precipitation_project():
+    return render_template('precipitation_project.html')
+
+@app.route('/cert1')
+def cert1():
+    return render_template('cert1.html')  
+
+@app.route('/cert2')
+def cert2():
+    return render_template('cert2.html') 
+
+@app.route('/cert3')
+def cert3():
+    return render_template('cert3.html') 
+
+@app.route('/cert4')
+def cert4():
+    return render_template('cert4.html') 
+
+@app.route('/cert5')
+def cert5():
+    return render_template('cert5.html') 
+
+if __name__ == '__main__':
+    app.run(debug=True)
